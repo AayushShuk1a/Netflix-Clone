b3e5578d6836056b8c25ec22d4618703c0eb8fdc
Aayush <aayush5emails@gmail.com>
2022-03-21 15:23:21 +0530

    Left Arrow Working

--- client/src/Components/List/List.jsx
+++ client/src/Components/List/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./list.scss";
 import {
   ArrowBackIosOutlined,
@@ -7,12 +7,24 @@ import {
 import ListItem from "../ListItem/ListItem";
 
 const List = () => {
+  const linkref = useRef();
+
+  const HandleClick = (direction) => {
+    const distance = linkref.current.getBoundingClientRect().x - 50;
+    if (direction === "left") {
+      linkref.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+  };
+
   return (
     <div className="list">
       <span className="ListTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" />
-        <div className="container">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => HandleClick("left")}
+        />
+        <div className="container" ref={linkref}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -42,7 +54,10 @@ const List = () => {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right" />
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => HandleClick("right")}
+        />
       </div>
     </div>
   );
