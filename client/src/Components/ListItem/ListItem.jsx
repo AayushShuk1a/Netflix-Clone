aa2ecb55afc861bbce02b15360962bdd596f0e9a
Aayush <aayush5emails@gmail.com>
2022-03-22 14:46:18 +0530

    Hover Edit

--- client/src/Components/ListItem/ListItem.jsx
+++ client/src/Components/ListItem/ListItem.jsx
@@ -9,6 +9,9 @@ import {
 
 const ListItem = ({ index }) => {
   const [IsHovered, setIsHovered] = useState(false);
+
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   console.log(IsHovered);
   return (
     <div
@@ -23,26 +26,32 @@ const ListItem = ({ index }) => {
     >
       <img src="https://wallpapercave.com/wp/wp3963420.jpg" alt="" />
 
-      <div className="ItemInfo">
-        <div className="Icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownOutlined />
-        </div>
-        <div className="ItemInfoTop">
-          <span>5 Season</span>
-          <span className="limit">18+</span>
-          <span>2008</span>
-        </div>
-        <div className="description">
-          Walter White, a chemistry teacher, discovers that he has cancer and
-          decides to get into the meth-making business to repay his medical
-          debts.
-        </div>
+      {IsHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop></video>
 
-        <div className="genre">Crime Drama</div>
-      </div>
+          <div className="ItemInfo">
+            <div className="Icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownOutlined className="icon" />
+            </div>
+            <div className="ItemInfoTop">
+              <span>5 Season</span>
+              <span className="limit">18+</span>
+              <span>2008</span>
+            </div>
+            <div className="description">
+              Walter White, a chemistry teacher, discovers that he has cancer
+              and decides to get into the meth-making business to repay his
+              medical debts.
+            </div>
+
+            <div className="genre">Crime Drama</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
