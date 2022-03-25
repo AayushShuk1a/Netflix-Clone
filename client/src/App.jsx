c636a00d9da91009dd72a840225ec972c537ce0e
Aayush <aayush5emails@gmail.com>
2022-03-25 21:17:53 +0530

    Login page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Components/Homepage/Home";
 import { Routes, Route } from "react-router-dom";
 import WatchPage from "./Components/Watch-Page/WatchPage";
 import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route exact path="/" element={<Home />}></Route>
       <Route exact path="/watch" element={<WatchPage />} />
       <Route exact path="/register" element={<Register />} />
+      <Route exact path="/login" element={<Login />}></Route>
     </Routes>
   );
 };
