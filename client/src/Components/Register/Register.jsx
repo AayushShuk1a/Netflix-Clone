db4591a99ffc671f74d327f6cb5ccb9ac264a615
Aayush <aayush5emails@gmail.com>
2022-03-25 21:09:20 +0530

    Register Page

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./register.scss";
 
 const Register = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const eref = useRef();
+  const pref = useRef();
+
+  const HandleStart = () => {
+    setEmail(eref.current.value);
+  };
+
+  console.log(Email);
+
+  const HandleFinish = () => {
+    setPassword(pref.current.value);
+  };
+
+  console.log(Password);
+
   return (
     <div className="register">
       <div className="top">
@@ -20,10 +37,31 @@ const Register = () => {
         <p>
           Ready to watch? Enter your email to create or restart your membership.
         </p>
-        <div className="input">
-          <input type="email" name="email" placeholder="Email Address" />
-          <button className="RegisterButton">Get Started</button>
-        </div>
+        {!Email ? (
+          <div className="input">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              ref={eref}
+            />
+            <button className="RegisterButton" onClick={HandleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <div className="input">
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              ref={pref}
+            />
+            <button className="RegisterButton" onClick={HandleFinish}>
+              Start
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
