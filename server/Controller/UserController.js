bc3ed5e69b7e8fa5d972150183840a07bb3593f2
Aayush <aayush5emails@gmail.com>
2022-04-02 02:28:37 +0530

    Crypto-js

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -1,15 +1,19 @@
 import User from "../Schema/user.js";
+import CryptoJS from "crypto-js";
 
 export const RegisterPerson = async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      JSON.stringify(req.body.password),
+      process.env.SECRET_KEY
+    ).toString(),
   });
 
   try {
     await newUser.save();
-    res.status(200), json(newUser);
+    res.status(200).json(newUser);
   } catch (error) {
     res.status(500).json(error);
   }
