88ecf422bc4cc259d2272207fcce144556859751
AayushShuk1a <aayush5emails@gmail.com>
2024-06-25 19:48:26 +0530

    Fixed Bugs

--- server/index.js
+++ server/index.js
@@ -6,27 +6,26 @@ import MovieRoute from "./Router/MovieRoutes.js";
 import ListRoute from "./Router/ListRoute.js";
 import cors from "cors";
 
-
 dotenv.config();
 
 const app = express();
+
 const corsOptions = {
-  origin: 'https://netflix-clone-client-tan.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: "https://netflix-clone-client-tan.vercel.app",
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
+
+
 const PORT = process.env.PORT || 8800;
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.use("/", Router);
 app.use("/api/movie", MovieRoute);
 app.use("/api/list", ListRoute);
+
 
 app.listen(PORT, () => {
   console.log("Server is Running");
