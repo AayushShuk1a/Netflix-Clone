316db2759c4ac3c22f52a8d84414ce9d1a156b78
AayushShuk1a <aayush5emails@gmail.com>
2024-06-25 19:51:53 +0530

    Added Middlewares

--- server/index.js
+++ server/index.js
@@ -18,6 +18,13 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://netflix-clone-client-tan.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 
 
 const PORT = process.env.PORT || 8800;
